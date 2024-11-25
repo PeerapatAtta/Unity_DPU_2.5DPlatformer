964a1b67f94615c5fcc142c8d0d21d9a7df32faf
OFF <peerapat13th@gmail.com>
2024-11-25 22:08:48 +0700

    4. Camera Control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,17 +13,20 @@ public class PlayerController : MonoBehaviour
 
     public CharacterController charController;
 
+    private Camera theCam;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        theCam = Camera.main; // get the main camera
     }
 
     // Update is called once per frame
     void Update()
     {
         float yStore = moveDirection.y; // store the y value of the move direction
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));// get the input from the player
+        // moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));// get the input from the player
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal")); // get the input from the player
         moveDirection = moveDirection * moveSpeed; // multiply the input by the move speed
         moveDirection.y = yStore; // set the y value of the move direction to the stored y value
 
@@ -36,5 +39,11 @@ public class PlayerController : MonoBehaviour
 
         // transform.position = transform.position + (moveDirection * Time.deltaTime * moveSpeed); // move the player
         charController.Move(moveDirection * Time.deltaTime); // move the player
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) // if the player is moving
+        {
+            transform.rotation = Quaternion.Euler(0f, theCam.transform.eulerAngles.y, 0f); // rotate the player to face the direction of the camera
+        }
+
     }
 }
